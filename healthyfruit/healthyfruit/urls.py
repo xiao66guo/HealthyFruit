fb278d958fc31a1691095396fba7f759b1e13d20
weixiaoguo <xiao66guo@163.com>
2018-06-05 15:39:50 +0800

    给每个应用模块添加urls文件并配置这些应用在主urls文件中的路径

--- healthyfruit/healthyfruit/urls.py
+++ healthyfruit/healthyfruit/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tinymce/', include('tinymce.urls')),  # 富文本编辑器
+    path('user/', include(('apps.user.urls', 'user'), namespace='user')),  # 用户模块
+    path('shop/', include(('apps.shop.urls', 'shop'), namespace='shop')),  # 购物车模块
+    path('order/', include(('apps.order.urls', 'order'), namespace='order')),  # 订单模块
+    path('goods/', include(('apps.goods.urls', 'goods'), namespace='goods')),  # 商品模块
+
 ]
