08231d18b891f95c7067b23d069ce23909c68e1c
weixiaoguo <xiao66guo@163.com>
2018-06-06 10:24:05 +0800

    解决as_view() takes 1 positional argument but 2 were given问题

--- healthyfruit/apps/user/urls.py
+++ healthyfruit/apps/user/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # path('register', views.register, name='register'),     # 注册页面
     # path('register_handle', views.register_handle, name='register_handle'),  # 注册处理
 
-    path('register', RegisterView.as_view, name='register'),     # 注册页面
+    path('register', RegisterView.as_view(), name='register'),     # 注册页面
 ]
