2ab0158eed26bb9afed25b3738b159337aafc32c
weixiaoguo <xiao66guo@163.com>
2018-06-06 10:19:55 +0800

    通过类视图实现注册及注册业务处理

--- healthyfruit/apps/user/urls.py
+++ healthyfruit/apps/user/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from user import views
+from user.views import RegisterView
 
 urlpatterns = [
-    path('register', views.register, name='register'),     # 注册页面
+    # path('register', views.register, name='register'),     # 注册页面
     # path('register_handle', views.register_handle, name='register_handle'),  # 注册处理
 
-
+    path('register', RegisterView.as_view, name='register'),     # 注册页面
 ]
--- healthyfruit/apps/user/views.py
+++ healthyfruit/apps/user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpResponse
+from django.views.generic import View
 from user.models import User
 import re
 
@@ -48,4 +49,44 @@ def register(request):
         return redirect(reverse('goods:index'))
 
 
+# 创建类视图
+class RegisterView(View):
+    # 显示注册页面
+    def get(self, request):
+        return render(request, 'register.html')
+
+    # 注册处理
+    def post(self, request):
+        # 接收数据
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+
+        # 进行校验(校验数据的完整性)
+        if not all([username, password, email]):
+            return render(request, 'register.html', {'error': '对不起，您输入的内容不完整！😭😭😭'})
+
+        # 校验邮箱的正确性
+        if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
+            return render(request, 'register.html', {'error': '对不起，您输入的邮箱不正确！😭😭😭'})
+
+        # 对注册的用户名进行判断是否已经存在
+        try:
+            user_info = User.objects.get(username=username)
+        except User.DoesNotExist:
+            # 用户名不存在
+            user_info = None
+
+        if user_info:
+            # 用户名已存在
+            return render(request, 'register.html', {'error': '对不起，您输入的用户名已存在！😭😭😭'})
+
+        # 业务处理:进行注册操作
+        # User.objects.create(username=username, password=password, email=email)
+        user_info = User.objects.create_user(username, password, email)
+        user_info.is_active = 0
+        user_info.save()
+
+        # 注册成功后跳转页面
+        return redirect(reverse('goods:index'))
 
