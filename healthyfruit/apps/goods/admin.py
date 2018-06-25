59b02e31ebd8068e4f2ce65f0558e2eca49ce09d
weixiaoguo <xiao66guo@163.com>
2018-06-25 12:23:14 +0800

    生成静态首页

--- healthyfruit/apps/goods/admin.py
+++ healthyfruit/apps/goods/admin.py
@@ -1,5 +1,43 @@
 from django.contrib import admin
-from goods.models import GoodsType
+from goods.models import GoodsType, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
+
+class BaseAdmin(admin.ModelAdmin):
+    # 增加数据或者更新数据是被调用
+    def save_model(self, request, obj, form, change):
+        # 执行增加或者更新操作
+        super().save_model(request, obj, form, change)
+
+        # 启动celery
+        from celery_tasks.task import generate_static_index
+        print('更新 generate_static_index.delay')
+        generate_static_index.delay()
+
+    # 删除数据时调用
+    def delete_model(self, request, obj):
+        super().delete_model(request, obj)
+
+        # 启动celery
+        from celery_tasks.task import generate_static_index
+        generate_static_index.delay()
 
 
-admin.site.register(GoodsType)
+class GoodsTypeAdmin(BaseAdmin):
+    pass
+
+
+class IndexGoodsBannerAdmin(BaseAdmin):
+    pass
+
+
+class IndexPromotionBannerAdmin(BaseAdmin):
+    pass
+
+
+class IndexTypeGoodsBannerAdmin(BaseAdmin):
+    pass
+
+
+admin.site.register(GoodsType, GoodsTypeAdmin)
+admin.site.register(IndexTypeGoodsBanner, IndexTypeGoodsBannerAdmin)
+admin.site.register(IndexPromotionBanner, IndexPromotionBannerAdmin)
+admin.site.register(IndexGoodsBanner, IndexGoodsBannerAdmin)
--- healthyfruit/celery_tasks/task.py
+++ healthyfruit/celery_tasks/task.py
@@ -1,10 +1,12 @@
 from celery import Celery
 from django.conf import settings
 from django.core.mail import send_mail
+from django.template import loader
 import os
 # 设置 Django 配置依赖的环境变量
+import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "healthyfruit.settings")
-
+django.setup()
 # 创建一个 Celery 对象
 app = Celery('celery_tasks.tasks', broker='redis://127.0.0.1:6379/11')
 
@@ -18,3 +20,42 @@ def send_register_active_email(to_email, username, token):
     sender_user = settings.EMAIL_FROM
     receiver = [to_email]
     send_mail(subject, message, sender_user, receiver, html_message=html_message)
+
+
+from goods.models import GoodsType, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
+# 生成商品首页静态页面
+@app.task
+def generate_static_index():
+    # 获取商品分类信息
+    types = GoodsType.objects.all()
+    # 获取首页轮播商品信息
+    scroll_banner = IndexGoodsBanner.objects.all().order_by( 'index' )
+    # 获取首页促销活动信息
+    promotion_banner = IndexPromotionBanner.objects.all().order_by( 'index' )
+    # 获取首页分类商品展示信息
+    for type in types:
+        # 查询type种类首页展示的文字商品
+        title_banner = IndexTypeGoodsBanner.objects.filter( type=type, display_type=0 ).order_by( 'index' )
+        # 查询type种类首页展示的图片image善品信息
+        image_banner = IndexTypeGoodsBanner.objects.filter( type=type, display_type=1 ).order_by( 'index' )
+        type.title_banner = title_banner
+        type.image_banner = image_banner
+    # 获取登录用户的购物车信息
+    shop_count = 0
+
+    # 使用模板，渲染生成静态首页的内容
+    # 1.加载模板文件，获取模板对象
+    temp = loader.get_template('static_index.html')
+    # 2.模板上下文
+    context = {'types': types,
+               'scroll_banner': scroll_banner,
+               'promotion_banner': promotion_banner,
+               'shop_count': shop_count}
+    # 3.模板渲染，生成HTML文件
+    static_index = temp.render(context)
+
+    # 保存一个首页静态文件
+    save_path = os.path.join(settings.BASE_DIR, 'static/index.html')
+    with open(save_path, 'w') as f:
+        f.write(static_index)
+
