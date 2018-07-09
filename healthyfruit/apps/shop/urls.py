bcc57f608e08cfeff872f0fc238030065cd3ab05
weixiaoguo <xiao66guo@163.com>
2018-07-09 17:24:57 +0800

    对购物车中商品记录总数量和总价格的增加进行同步处理

--- healthyfruit/apps/shop/urls.py
+++ healthyfruit/apps/shop/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from shop.views import ShopAddView, ShopInfoView
+from shop.views import ShopAddView, ShopInfoView, ShopUpdateView
 
 urlpatterns = [
     # path('detail/<sku_id>', DetailView.as_view(), name='detail'),
-    path('add', ShopAddView.as_view(), name='add'),         # 购物车记录的添加
-    path('show', ShopInfoView.as_view(), name='show'),      # 购物车页面显示
+    path('add', ShopAddView.as_view(), name='add'),             # 购物车记录的添加
+    path('show', ShopInfoView.as_view(), name='show'),          # 购物车页面显示
+    path('update', ShopUpdateView.as_view(), name='update'),    # 购物车数据更新
 
 ]
--- healthyfruit/apps/shop/views.py
+++ healthyfruit/apps/shop/views.py
@@ -5,7 +5,7 @@ from django_redis import get_redis_connection
 from goods.models import GoodsSKU
 from utils.mini import LoginRequiredView
 
-# Create your views here.
+# 采用ajax post请求（商品ID<sku_id> 和 更新数量<count>）
 # 购物车记录的添加
 class ShopAddView(View):
     def post(self, request):
@@ -20,6 +20,7 @@ class ShopAddView(View):
 
         # 对接收到的参数进行校验
         if not all([sku_id, count]):
+            print(sku_id, count)
             return JsonResponse({'res': 1, 'msg': '数据不完整'})
 
         # 对商品的id进行判断
@@ -58,6 +59,7 @@ class ShopAddView(View):
         return JsonResponse({'res': 5, 'shop_count': shop_count, 'msg': '添加成功'})
 
 
+# 采用ajax post请求（商品ID<sku_id> 和 更新数量<count>）
 # 购物车页面的信息
 class ShopInfoView(LoginRequiredView, View):
     def get(self, request):
@@ -92,4 +94,58 @@ class ShopInfoView(LoginRequiredView, View):
                    'total_amount': total_amount}
 
         return render(request, 'cart.html', context)
+
+
+# 采用ajax post请求（商品ID<sku_id> 和 更新数量<count>）
+# 购物车页面信息更新
+class ShopUpdateView(View):
+    def post(self, request):
+        # 获取登录的用户
+        user = request.user
+        if not user.is_authenticated:
+            return JsonResponse({'res': 1, 'msg': '用户为登录'})
+
+        # 接收参数
+        sku_id = request.POST.get('sku_id')
+        count = request.POST.get('count')
+        
+        # 对接收到的参数进行校验
+        if not all([sku_id, count]):
+
+            return JsonResponse({'res': 1, 'msg': '数据不完整'})
+
+        # 对商品的id进行判断
+        try:
+            sku = GoodsSKU.objects.get(id=sku_id)
+        except GoodsSKU.DoesNotExist:
+            return JsonResponse({'res': 2, 'msg': '数据不存在'})
+
+        # 对商品的数量进行判断
+        try:
+            count = int(count)
+        except Exception as Error:
+            return JsonResponse({'res': 3, 'msg': '商品数目非法'})
+        if count <= 0:
+            return JsonResponse({'res': 3, 'msg': '商品数目非法'})
+
+        # 更新用户购物车记录信息
+        # 获取链接
+        conn = get_redis_connection('default')
+        # 拼接key
+        shop_key = 'shop_%d' % user.id
+        # 对商品的库存进行判断
+        if count > sku.stock:
+            return JsonResponse({'res': 4, 'msg': '商品库存不足'})
+
+        # 更新购物车中商品的数目
+        conn.hset(shop_key, sku_id, count)
+
+        shop_count = 0
+        shop_vals = conn.hvals(shop_key)
+        for val in shop_vals:
+            shop_count += int(val)
+
+
+
+        return JsonResponse({'res': 5, 'shop_count': shop_count, 'msg': '更新成功'})
 
