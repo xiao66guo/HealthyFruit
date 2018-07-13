79dd8444ff05f09d8ac47bd4d6ab38664a83c188
weixiaoguo <xiao66guo@163.com>
2018-07-13 19:08:03 +0800

    对用户购买商品的订单进行创建

--- healthyfruit/apps/order/urls.py
+++ healthyfruit/apps/order/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from order.views import OrderPlaceView
+from order.views import OrderPlaceView, OrderCommitView
 
 urlpatterns = [
     path('place', OrderPlaceView.as_view(), name='place'),          # 提交订单页面的显示
-
+    path('commit', OrderCommitView.as_view(), name='commit'),       # 订单的创建
 
 
 ]
