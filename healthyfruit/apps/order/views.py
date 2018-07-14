50dd4a624b11bd5f857bad06fc574fe5bf9428b0
weixiaoguo <xiao66guo@163.com>
2018-07-14 11:25:04 +0800

    用数据库中的事务回滚来对在提交订单时出现的商品库存不足问题进行解决

--- healthyfruit/apps/order/views.py
+++ healthyfruit/apps/order/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.urls import reverse
+from django_redis import get_redis_connection
+from django.http import JsonResponse
+from django.db import transaction
+
 from user.models import Address
 from utils.mini import LoginRequiredView
 from goods.models import GoodsSKU
-from django_redis import get_redis_connection
-from django.http import JsonResponse
 from order.models import OrderInfo, OrderGoods
 from datetime import datetime
 
@@ -71,6 +73,7 @@ class OrderPlaceView(LoginRequiredView, View):
 
 # 创建订单
 class OrderCommitView(View):
+    @transaction.atomic()
     def post(self, request):
         # 对登录用户进行判断
         user = request.user
@@ -104,47 +107,61 @@ class OrderCommitView(View):
         # 订单中商品的总数量和总价格
         total_count = 0
         total_price = 0
-        '''向 df_order_info 中添加一条记录'''
-        order = OrderInfo.objects.create(order_id=order_id,
-                                         user=user,
-                                         addr=addr,
-                                         pay_method=pay_method,
-                                         total_count=total_count,
-                                         total_price=total_price,
-                                         transit_price=trans_price)
 
-        # 获取redis的链接对象
-        conn = get_redis_connection('default')
-        shop_key = 'shop_%d' % user.id
+        # 设置mysql事务的保存点
+        sid = transaction.savepoint()
+        try:
+            '''向 df_order_info 中添加一条记录'''
+            order = OrderInfo.objects.create(order_id=order_id,
+                                             user=user,
+                                             addr=addr,
+                                             pay_method=pay_method,
+                                             total_count=total_count,
+                                             total_price=total_price,
+                                             transit_price=trans_price)
 
-        sku_ids = sku_ids.split(',')
-        # 遍历获取用户所要购买的商品信息
-        for sku_id in sku_ids:
-            # 根据id查询商品的信息
-            try:
-                sku = GoodsSKU.objects.get(id=sku_id)
-            except GoodsSKU.DoesNotExist:
-                return JsonResponse({'res': 4, 'msg': '商品信息错误'})
+            # 获取redis的链接对象
+            conn = get_redis_connection('default')
+            shop_key = 'shop_%d' % user.id
 
-            # 从redis中获取用户要购买的商品数量
-            count = conn.hget(shop_key, sku_id)
+            sku_ids = sku_ids.split(',')
+            # 遍历获取用户所要购买的商品信息
+            for sku_id in sku_ids:
+                # 根据id查询商品的信息
+                try:
+                    sku = GoodsSKU.objects.get(id=sku_id)
+                except GoodsSKU.DoesNotExist:
+                    # 将事务回滚到保存点之前
+                    transaction.savepoint_rollback(sid)
+                    return JsonResponse({'res': 4, 'msg': '商品信息错误'})
 
-            '''向 df_order_goods 中添加一条记录'''
-            OrderGoods.objects.create(order=order,
-                                      sku=sku,
-                                      count=int(count),
-                                      price=sku.price)
-            '''对商品的库存减少，销量增加'''
-            sku.stock -= int(count)
-            sku.sales += int(count)
-            '''对用户购买的商品的总数量和总价格进行累计'''
-            total_count += int(count)
-            total_price += sku.price * int(count)
+                # 从redis中获取用户要购买的商品数量
+                count = conn.hget(shop_key, sku_id)
 
-        # TODO:更新订单信息记录中购买的商品的总数目和总金额
-        order.total_count = total_count
-        order.total_price = total_price
-        order.save()
+                # 判断商品的库存
+                if sku.stock < int(count):
+                    transaction.savepoint_rollback(sid)
+                    return JsonResponse({'res': 6, 'msg': '商品库存不足'})
+
+                '''向 df_order_goods 中添加一条记录'''
+                OrderGoods.objects.create(order=order,
+                                          sku=sku,
+                                          count=int(count),
+                                          price=sku.price)
+                '''对商品的库存减少，销量增加'''
+                sku.stock -= int(count)
+                sku.sales += int(count)
+                '''对用户购买的商品的总数量和总价格进行累计'''
+                total_count += int(count)
+                total_price += sku.price * int(count)
+
+            # TODO:更新订单信息记录中购买的商品的总数目和总金额
+            order.total_count = total_count
+            order.total_price = total_price
+            order.save()
+        except Exception as e:
+            transaction.savepoint_rollback(sid)
+            return JsonResponse({'res': 7, 'msg': '下单失败'})
 
         # TODO:删除购物车中的记录
         conn.hdel(shop_key, *sku_ids)
